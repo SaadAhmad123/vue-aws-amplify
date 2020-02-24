d6be4faa7a5e2e24b5cdc317b40bcaae6eb47529
SaadAhmad123 <sahmad.bee15seecs@seecs.edu.pk>
2020-02-24 13:01:54 +1100

    HelloWorld

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <v-app> 
+      Hello
+    </v-app>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is the sign  up page
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signup',
+}
+</script>
+
+<style scoped>
+
+</style>
